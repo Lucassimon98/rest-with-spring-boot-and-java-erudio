d7dfbf73679d871421ba982afa1ae77a57272949
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-10-07 14:00:20 -0300

    Adicionando Migrations com Flyway

---
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `person` (
+  `id` bigint(20) NOT NULL,
+  `first_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `gender` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(3, 'Qualquer', 'Luis', 'male', 'Simon'),
+(5, 'Qualquer', 'Lucas', 'male', 'Simon'),
+(6, 'Qualquer', 'Nathalia', 'female', 'Simon'),
+(7, 'Qualquer', 'Heitor', 'male', 'Simon')
