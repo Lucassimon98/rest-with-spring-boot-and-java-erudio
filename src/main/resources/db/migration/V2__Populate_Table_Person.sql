bef13f808793c08e47d6a1ba8214311aab831b53
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-10-07 14:57:42 -0300

    Adicionado Content Negotiation provendo JSON, XML e YAML

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,5 +1,5 @@
 INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
 (3, 'Qualquer', 'Luis', 'male', 'Simon'),
 (5, 'Qualquer', 'Lucas', 'male', 'Simon'),
-(6, 'Qualquer', 'Nathalia', 'female', 'Simon'),
+(6, 'Qualquer', 'Nathalia', 'male', 'Simon'),
 (7, 'Qualquer', 'Heitor', 'male', 'Simon')
